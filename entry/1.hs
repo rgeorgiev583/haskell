f58ead9c7bcc770333c822ca90b93d8e7084ace0
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-06-14 04:31:07 +0300

    fix: corrected indentation issues

--- entry/1.hs
+++ entry/1.hs
@@ -1,15 +1,15 @@
 fastestRoute :: [(a, a)] -> (a, a)
 fastestRoute [] = []
-fastestRoute routes = shortestPathBFS routes 'H' 'L' ['H'] ['H']
-    where shortestPathBFS graph start end queue path
+fastestRoute routes = shortestPathBFS routes 'H' 'L' ['H'] ['H'] where
+    shortestPathBFS graph start end queue path
         | null queue    =  []
         | start == end  =  path
         | otherwise     =  head (filter (\next -> not null next) (map reccall (adjacentVertices graph vertex)))
-            where
-                vertex         =  head queue,
-                newQueue next  =  if elem next path then tail queue else (tail queue) ++ [next],
-                newPath  next  =  if elem next path then path       else path ++ [next],
-                reccall  next  =  shortestPathBFS graph next end (newQueue next) (newPath next)
+        where
+            vertex         =  head queue,
+            newQueue next  =  if elem next path then tail queue else (tail queue) ++ [next],
+            newPath  next  =  if elem next path then path       else path ++ [next],
+            reccall  next  =  shortestPathBFS graph next end (newQueue next) (newPath next)
 
 
 adjacentVertices :: [(a, a)] -> a -> [a]
