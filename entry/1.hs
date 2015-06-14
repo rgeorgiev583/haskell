247d4792fadf1de028a264730c72951044654cbd
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-06-14 04:32:08 +0300

    fix: wrong syntax: stripped commas between where definitions

--- entry/1.hs
+++ entry/1.hs
@@ -6,9 +6,9 @@ fastestRoute routes = shortestPathBFS routes 'H' 'L' ['H'] ['H'] where
         | start == end  =  path
         | otherwise     =  head (filter (\next -> not null next) (map reccall (adjacentVertices graph vertex)))
         where
-            vertex         =  head queue,
-            newQueue next  =  if elem next path then tail queue else (tail queue) ++ [next],
-            newPath  next  =  if elem next path then path       else path ++ [next],
+            vertex         =  head queue
+            newQueue next  =  if elem next path then tail queue else (tail queue) ++ [next]
+            newPath  next  =  if elem next path then path       else path ++ [next]
             reccall  next  =  shortestPathBFS graph next end (newQueue next) (newPath next)
 
 
