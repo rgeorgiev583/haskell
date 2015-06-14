41bc4fc88d4bf9575347400b8792a597effc4474
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-06-14 05:19:58 +0300

    fix: rewrote what was essentially DFS to actually be BFS: now checking list of adjacencies for presence of end vertex instead of recursing in depth; dropped start argument to BFS which was essentially not used

--- entry/1.hs
+++ entry/1.hs
@@ -1,15 +1,16 @@
 fastestRoute :: [(Char, Char)] -> [Char]
 fastestRoute [] = []
-fastestRoute routes = shortestPathBFS routes 'H' 'L' ['H'] ['H'] where
-    shortestPathBFS graph start end queue path
+fastestRoute routes = shortestPathBFS routes 'L' ['H'] ['H'] where
+    shortestPathBFS graph end queue path
         | null queue    =  []
-        | start == end  =  path
-        | otherwise     =  head (filter (\next -> not (null next)) (map reccall (adjacentVertices graph vertex)))
+        | elem end adjacencies  =  path ++ [end]
+        | otherwise     =  head (filter (\next -> not (null next)) (map reccall adjacencies))
         where
             vertex         =  head queue
+            adjacencies    =  adjacentVertices graph vertex
             newQueue next  =  if elem next path then tail queue else (tail queue) ++ [next]
             newPath  next  =  if elem next path then path       else path ++ [next]
-            reccall  next  =  shortestPathBFS graph next end (newQueue next) (newPath next)
+            reccall  next  =  shortestPathBFS graph end (newQueue next) (newPath next)
 
 
 adjacentVertices :: Eq a => [(a, a)] -> a -> [a]
